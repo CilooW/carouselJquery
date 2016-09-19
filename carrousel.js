88260e33de85e194887bc7cb7458d1e95e591f10
CilooW <cecile.angays@yahoo.fr>
2016-09-19 19:01:57 +0200

    enfin finiiiiiiiii(ceci est un mauvais message de commit)

--- carrousel.js
+++ carrousel.js
@@ -4,10 +4,33 @@ $(document).ready(function() {
         var $slider = $('.slider'); // définit une variable avec la class attribuée au carousel > sélectionne l'élément HTML avec la class .slider
         var $slide = 'li'; // ou img si on n'a pas fait de liste > sélectionne chaque li
         var $transition_time = 1000; // 1 seconde pour passe d'un slide à un autre
-        var $time_between_slides = 3000; // reste 3 secondes
+        var $time_between_slides = 1000; // reste 3 secondes
+
 
         function slides(){
             return $slider.find($slide); // définit une fonction slides qui trouve le nombre de slide en comptant dans .slider combien il trouve de li
+        }
+
+        function moveSlide(value){
+
+            var currentSlide = $slider.find($slide + '.active').index();   // définit une variable i qui correspond au numéro d'index de la li dans la class .slider qui a la class active.
+            var nextSlide = currentSlide + value;                                                     // ça revient à trouver l'image active.
+
+            if (nextSlide > slides().length - 1) {
+                nextSlide = 0;
+            }
+            if (nextSlide < 0) {
+                nextSlide = slides().length - 1;
+            }
+
+            slides().eq(currentSlide).removeClass('active');              // sélectionne la slide actuelle (i étant l'index de la slide qui a la class active) et lui enlève la class active
+            slides().eq(currentSlide).fadeOut($transition_time);          // slide actuelle > effet de fadeOut avec le temps défini dans la transition)
+
+
+            //if (slides().length == $i + 1) $i = -1; // si le nombre total de slide (défini par la fonction slides) est égal à l'index actif +1, alors on réinitialise i à -1.
+
+            slides().eq(nextSlide).fadeIn($transition_time); // ajoute +1 à la slide actuelle (si c'est à -1, repart à 0, sinon, incrémente de 1) et ajoute un fade in avec temps de transi défini
+            slides().eq(nextSlide).addClass('active'); // ajoute la class active à cette nouvelle slide
         }
 
         slides().fadeOut(); // lance la fonction slides et anime avec un fadeOut.
@@ -18,19 +41,22 @@ $(document).ready(function() {
 
         // auto scroll > défilement automatique des images
         $interval = setInterval(
-            function(){
-                var $i = $slider.find($slide + '.active').index();   // définit une variable i qui correspond au numéro d'index de la li dans la class .slider qui a la class active.
-                                                                     // ça revient à trouver l'image active.
-
-                slides().eq($i).removeClass('active');              // sélectionne la slide actuelle (i étant l'index de la slide qui a la class active) et lui enlève la class active
-                slides().eq($i).fadeOut($transition_time);          // slide actuelle > effet de fadeOut avec le temps défini dans la transition)
-
-                if (slides().length == $i + 1) $i = -1; // si le nombre total de slide (défini par la fonction slides) est égal à l'index actif +1, alors on réinitialise i à -1.
-
-                slides().eq($i + 1).fadeIn($transition_time); // ajoute +1 à la slide actuelle (si c'est à -1, repart à 0, sinon, incrémente de 1) et ajoute un fade in avec temps de transi défini
-                slides().eq($i + 1).addClass('active'); // ajoute la class active à cette nouvelle slide
+            function (){
+                moveSlide(1);
             }
             , $transition_time +  $time_between_slides //attends le temps de transition des slides + le temps entre deux slide définit en haut
         );
+
+       $('#prev').click(function() {
+            //alert("coucou");
+            moveSlide(-1);
+        })
+
+
+
+        $('#next').click(function(){
+            moveSlide(1);
+        })
+
 
     });
